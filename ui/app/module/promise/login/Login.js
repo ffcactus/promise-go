770b1ce1ec74881d5a48e78d8566da63d38eb8af
ffcactus <ffcactus@me.com>
2018-02-27 22:19:11 +0800

    Bug fix.
    Fix a bug that if the user go to /login directly.

--- ui/app/module/promise/login/Login.js
+++ ui/app/module/promise/login/Login.js
@@ -37,7 +37,8 @@ class Login extends Component {
     event.preventDefault();
 
     // If we can't find a next path after login, we go to root.
-    this.props.dispatch(login(this.state.hostname, this.state.username, this.state.password, this.props.location.state.from));
+    let from = this.props.location.state ? this.props.location.state.from : '/';
+    this.props.dispatch(login(this.state.hostname, this.state.username, this.state.password, from));
   }
 
 
