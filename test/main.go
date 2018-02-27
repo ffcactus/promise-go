bd7f0d432ae8e73ee5cc798cdaf27c327e1a7375
ffcactus <ffcactus@me.com>
2018-02-27 14:06:55 +0800

    Add test project.

---
+++ test/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/astaxie/beego"
+	"net/http"
+	"time"
+)
+
+type AddServerRequest struct {
+	Address  string `json:"address"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func main() {
+	var (
+		count  int          = 100
+		Client *http.Client = &http.Client{}
+	)
+
+	for i := 0; i < count; i++ {
+		time.Sleep(time.Duration(0) * time.Millisecond)
+		address := "Mock" + Logger.Sprintf("%05d", i)
+		dto := AddServerRequest{
+			Address: address,
+		}
+		b := new(bytes.Buffer)
+		json.NewEncoder(b).Encode(dto)
+		req, _ := http.NewRequest(http.MethodPost, "http://localhost:8080/director/rich/v1/server", b)
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Accept", "application/json")
+		if resp, err := Client.Do(req); err != nil {
+			beego.Info("Post server %s failed. error = %s, response = %#v\n", address, err, resp)
+		} else {
+			resp.Body.Close()
+			if resp.StatusCode != http.StatusOK {
+				beego.Info("post server %s failed, status code = %d\n", address, resp.StatusCode)
+			} else {
+				beego.Info("post server %s done.\n", address)
+			}
+		}
+	}
+}
