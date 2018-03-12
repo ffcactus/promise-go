ce699f4c0f3ddd71c8bacc76c1c2e6bd42014498
ffcactus <ffcactus@me.com>
2018-03-12 12:10:50 +0800

    Remove unnecessary files.

--- integration-test/src/test/java/com/promise/integrationtest/PromiseTestBase.java
+++
@@ -1,9 +0,0 @@
-package com.promise.integrationtest;
-
-public class PromiseTestBase
-{
-    public static final String HOSTNAME = "http://192.168.116.137";
-    public static final String USERNAME = "Administrator";
-    public static final String PASSWORD = "admin";
-    public static final String DOMAIN = "local";
-}
