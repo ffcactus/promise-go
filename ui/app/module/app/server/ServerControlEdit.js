0a73213733e4cf42229dfc463ff65272e9161b03
ffcactus <ffcactus@me.com>
2018-09-25 22:22:49 +0800

    Update edit icon.

--- ui/app/module/app/server/ServerControlEdit.js
+++ ui/app/module/app/server/ServerControlEdit.js
@@ -15,7 +15,7 @@ class ServerControlEdit extends React.Component {
   }
 
   render() {
-    const icon = require('../../promise/common/img/icon/Navigation_Add_2x.png');
+    const icon = require('../../promise/common/img/icon/Navigation_Compose_2x.png');
     return (
       <div styleName="ListControlAreaButton" style={{float: 'right'}}>
         <CenterDiv><img src={icon} onClick={this.onClick} style={{display: 'block', margin: 'auto', height: '30px'}}/></CenterDiv>
